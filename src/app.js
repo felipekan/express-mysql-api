907c033f35711b5e949b4b2bea1efcc6ecfef663
felipekan <felipekan17@gmail.com>
2022-12-13 23:52:08 -0300

    Project initial config

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import employeesRoutes from './routes/employees.routes.js'
+
+const app = express()
+app.use(express.json())
+
+app.use('/api', employeesRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' })
+})
+
+export default app
